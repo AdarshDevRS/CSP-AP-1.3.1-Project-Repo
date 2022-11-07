b04bd27985a9a65206e21f9b9367a45c2caf6791
AdarshDev <adarsh.sundaram3@gmail.com>
2022-11-07 17:37:17 -0500

    Version 0.0.0
    Everything is set up in place

---
+++ src/main.py
@@ -0,0 +1,25 @@
+#--- Imports ---#
+import turtle as trtl
+import random as rand
+
+
+#--- Obj ---#
+
+
+#--- Config ---#
+
+
+#--- Variables ---#
+
+
+#--- Functions ---#
+
+
+#--- Function Calls --#
+
+
+#--- Events ---#
+
+
+wn = trtl.Screen()
+wn.mainloop()
