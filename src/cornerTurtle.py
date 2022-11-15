07227b329565191424a9405496a5517cb3f5e89e
AdarshDev <adarsh.sundaram3@gmail.com>
2022-11-15 17:57:18 -0500

    Version 1.0.1
    Removed unnecessary code.

--- src/cornerTurtle.py
+++ src/cornerTurtle.py
@@ -5,12 +5,6 @@ import random as rand
 colors = ["red", "purple", "green", "blue"] 
 
 def initCornerTurtles(t1, t2, t3, t4):
-  turtles = [t1, t2, t3, t4]
-  
-  for t in turtles:
-    t.penup()
-    t.shape("circle")
-    
   t1.goto(-190, 190)
   t2.goto(190,190)
   t3.goto(-190, -190)
